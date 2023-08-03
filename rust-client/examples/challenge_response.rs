f588dbca983ce5029953b135f0ec6382f6191325
Dave Chen <dave.jungler@gmail.com>
2023-08-03 19:35:45 +0800

    feat: async processing
    Signed-off-by: Dave Chen <dave.jungler@gmail.com>

--- rust-client/examples/challenge_response.rs
+++ rust-client/examples/challenge_response.rs
@@ -5,19 +5,27 @@ extern crate veraison_apiclient;
 
 use veraison_apiclient::*;
 
-fn my_evidence_builder(nonce: &[u8], accept: &[String]) -> Result<(Vec<u8>, String), Error> {
+fn my_evidence_builder(
+    nonce: &[u8],
+    accept: &[String],
+    token: Vec<u8>,
+) -> Result<(Vec<u8>, String), Error> {
     println!("server challenge: {:?}", nonce);
     println!("acceptable media types: {:#?}", accept);
-
-    Ok((
+    let mut token = token;
+    if token.is_empty() {
         // some very fake evidence
-        vec![0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff],
+        token = vec![0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff];
+    }
+    Ok((
+        token,
         // the first acceptable evidence type
         accept[0].to_string(),
     ))
 }
 
-fn main() {
+#[async_std::main]
+async fn main() {
     let base_url = "http://127.0.0.1:8080";
 
     let discovery = Discovery::from_base_url(String::from(base_url))
@@ -25,6 +33,7 @@ fn main() {
 
     let verification_api = discovery
         .get_verification_api()
+        .await
         .expect("Failed to discover the verification endpoint details.");
 
     let relative_endpoint = verification_api
@@ -42,7 +51,7 @@ fn main() {
     let nonce = Nonce::Value(vec![0xde, 0xad, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xef]);
     // alternatively, to let Veraison pick the challenge: "let nonce = Nonce::Size(32);"
 
-    match cr.run(nonce, my_evidence_builder) {
+    match cr.run(nonce, my_evidence_builder, Vec::new()).await {
         Err(e) => println!("Error: {}", e),
         Ok(attestation_result) => println!("Attestation Result: {}", attestation_result),
     }
